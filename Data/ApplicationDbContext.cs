5b9af26f8d8449dcb63277b14dca3c3da38598f5
me.ghafari <me.ghafari@ME-GHAFARI>
2022-07-07 17:21:15 +0430

    add dbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,22 @@
 
+using InventoryMVC.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace InventoryMVC.Data{
     public class ApplicationDbContext: DbContext{
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options){
+        // public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        // : base(options){
             
+        // }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options){}
+
+        public DbSet<CustomerModel> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test123;Integrated Security=True");
         }
     }
 }
